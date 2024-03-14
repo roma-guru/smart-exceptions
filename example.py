efc758c881edfe8ce68bbc22304fb28daf01edf9
Roman Voropaev <voropaev.roma@gmail.com>
2024-03-14 13:52:19 +0200

    Linting

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
-import os
 import smart_exceptions as se
 
 se.init(lang="russian")
 se.install_handler()
 
-#3 / 0
-#print({}["asdf"])
+# 3 / 0
+# print({}["asdf"])
+
 
 def factorial(n):
-    return n*factorial(n-1)
+    return n * factorial(n - 1)
+
 
 print(factorial(100))
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     install_requires=[
         "openai==1.12.0",
-        #"google-generativeai==0.4.1",
+        # "google-generativeai==0.4.1",
         "anthropic==0.20.0",
         "rich~=13.7",
         'importlib-metadata; python_version>"3.9"',
